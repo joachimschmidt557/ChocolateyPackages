dd50d68f05f71a9ef98033d5669fc6d2cae49a5d
joachimschmidt557 <joachim.schmidt557@outlook.com>
2017-10-30 22:54:09 +0800

    WIP

--- mosh-for-chrome/tools/chocolateyuninstall.ps1
+++ mosh-for-chrome/tools/chocolateyuninstall.ps1
@@ -1,9 +1,10 @@
 $bits = Get-ProcessorBits
-$packageName = 'adblockpluschrome'
-$extensionID = 'cfhdojbkjhnklbpkdaibdccddilifddb'
+$packageName = 'mosh-for-chrome'
+$extensionID = 'ooiklbnjmhbcgemelgfhaeaocllobloj'
 
 if ($bits -eq 64) {
     Remove-Item "HKLM:\SOFTWARE\Wow6432node\Google\Chrome\Extensions\$extensionID" -Force -ErrorAction SilentlyContinue
-   }else{
+}
+else {
     Remove-Item "HKLM:\SOFTWARE\Google\Chrome\Extensions\$extensionID" -Force -ErrorAction SilentlyContinue
 }
