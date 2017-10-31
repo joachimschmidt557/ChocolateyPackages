e75336453a94a956650f4dac76566de17c2d82bf
joachimschmidt557 <joachim.schmidt557@outlook.com>
2017-10-31 10:06:12 +0800

    WIP

--- ArecaBackup/tools/chocolateyInstall.ps1
+++ ArecaBackup/tools/chocolateyInstall.ps1
@@ -1,8 +1,25 @@
-﻿$packageName = 'ArecaBackup'
-$installerType = 'EXE'
-$url = 'http://sourceforge.net/projects/areca/files/areca-stable/areca-7.4.8/areca-7.4.8-windows-jre32-setup.exe/download' # download url
-$url64 = 'http://sourceforge.net/projects/areca/files/areca-stable/areca-7.4.8/areca-7.4.8-windows-jre64-setup.exe/download' # 64bit URL here or remove - if installer decides, then use $url
-$silentArgs = '/S'
-$validExitCodes = @(0)
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://sourceforge.net/projects/areca/files/areca-stable/areca-7.5/areca-7.5-windows-jre32-setup.exe/download'
+$url64      = 'https://sourceforge.net/projects/areca/files/areca-stable/areca-7.5/areca-7.5-windows-jre64-setup.exe/download'
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'ArecaBackup*'
+
+  checksum      = 'E168FE78E53B4F208AA0A5F5353DCB3A64B2F1E17C88FCED7F29E2CD5909526A'
+  checksumType  = 'sha256'
+  checksum64    = '876190869F454D1EFEDD0E2D57781EA738C70E7CFBD82EC60BB44EB9CB1B83FE'
+  checksumType64= 'sha256'
+
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
--- ArecaBackup/tools/chocolateyUninstall.ps1
+++ ArecaBackup/tools/chocolateyUninstall.ps1
@@ -1,16 +1,32 @@
-try {
-  $processor = Get-WmiObject Win32_Processor
-  $is64bit = $processor.AddressWidth -eq 64
-  if ($is64bit) {
-    $unpath = "${Env:ProgramFiles(x86)}\Areca\uninstall.exe"
-  } else {
-    $unpath = "${Env:ProgramFiles}\Areca\uninstall.exe"
+﻿
+$ErrorActionPreference = 'Stop';
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  softwareName  = 'ArecaBackup*'
+  fileType      = 'EXE'
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+}
+
+$uninstalled = $false
+[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+if ($key.Count -eq 1) {
+  $key | % { 
+    $packageArgs['file'] = "$($_.UninstallString)"
+    if ($packageArgs['fileType'] -eq 'MSI') {
+      $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
+      
+      $packageArgs['file'] = ''
+    }
+
+    Uninstall-ChocolateyPackage @packageArgs
   }
-  Uninstall-ChocolateyPackage 'ArecaBackup' 'EXE' '/S' "$unpath" -validExitCodes @(0)
-  
-  # the following is all part of error handling
-  Write-ChocolateySuccess 'ArecaBackup'
-} catch {
-  Write-ChocolateyFailure 'ArecaBackup' "$($_.Exception.Message)"
-  throw
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
 }
